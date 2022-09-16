84271f3cd20907db8a28d4b486c4db5a693ea395
Dilsithmi Abeywickrama <2019cs032@stu.ucsc.lk>
2022-09-17 00:31:43 +0530

    first commit

---
+++ my_flutter_application_1/lib/home.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
+    Firebase.initializeApp();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firebase Demo"),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("Signed in"),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text('Sign out'),
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 173, 11, 92),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 11, vertical: 11),
+                    textStyle: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+            ]),
+      ),
+    );
+  }
+}
